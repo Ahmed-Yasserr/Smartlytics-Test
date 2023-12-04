e2abfd0040d02695803a8044a26f94852b069bd1
Ahmed-Yasserr <ahmed.yasser@smartlytics.co.uk>
2023-12-04 20:17:47 +0000

    Add Static Data

---
+++ SQL Logs/Tables/StaticData/dbo.ActionDetailIDs.Data.sql
@@ -0,0 +1,29 @@
+﻿INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (0, 'Logout', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (1, 'Picking Only
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (2, 'Labelling', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (3, 'Lunch', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (4, 'Break', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (5, 'Admin', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (6, 'Pick & Packing & Scanning
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (7, 'Meeting / Briefing
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (8, 'Dispatching
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (9, 'Cleaning
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (10, 'Repairs
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (11, 'Booking In
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (12, 'New Product Allocation
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (13, 'Packing & Scanning
+', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (14, 'Log In', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (15, 'Parts', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (16, 'Out Of Stock Scanning', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (17, 'Stock Take', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (18, 'Training', 1);
+INSERT dbo.ActionDetailIDs(CurrentActionID, ActionName, Active) VALUES (19, 'Royal Mail', 1);
---
+++ SQL Logs/Tables/StaticData/dbo.SampleWFA_UserGroupsToInclude.Data.sql
@@ -0,0 +1,2 @@
+﻿INSERT dbo.SampleWFA_UserGroupsToInclude(UserGroupID, Active) VALUES (2, 1);
+INSERT dbo.SampleWFA_UserGroupsToInclude(UserGroupID, Active) VALUES (5, 0);
