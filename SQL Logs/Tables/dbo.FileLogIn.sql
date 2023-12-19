43c5a1cf8e989e6ec73d4c4102dbe9ab2b8a2c8e
Ahmed-Yasserr <ahmed.yasser@smartlytics.co.uk>
2023-12-19 12:45:32 +0000

    Changed column to be nullable

--- SQL Logs/Tables/dbo.FileLogIn.sql
+++ SQL Logs/Tables/dbo.FileLogIn.sql
@@ -3,7 +3,7 @@
   [UserID] [int] NOT NULL,
   [ActionID] [int] NOT NULL,
   [FileComplete] [bit] NULL,
-  [TimeStarted] [datetime] NOT NULL,
+  [TimeStarted] [datetime] NULL,
   [TimeEnded] [datetime] NULL,
   [FileName] [varchar](50) NOT NULL,
   CONSTRAINT [PK_FileLogIn] PRIMARY KEY CLUSTERED ([FileID])
